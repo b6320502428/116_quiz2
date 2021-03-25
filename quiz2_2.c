4430b7fef35464790568c3bf1e9734faaf1134e2
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-25 18:13:17 +0700

    quiz2_2 up1

--- quiz2_2.c
+++ quiz2_2.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 int main ()
 {
-    int n,m;
-    scanf("%d %d",&n,&m);
+    int n,m,i,p,q,r,j;
+    scanf("%d %d",&m,&n);
+    char cloth[m][70];
+    for(i=0; i<m; i++)
+    {
+        for(j=0; j<70; j++)
+        {
+            cloth[i][j]='o';
+        }
+    }
+    for(i=0; i<n; i++)
+    {
+        scanf("%d %d %d",&p,&q,&r);
+    }
     return 0;
 }
