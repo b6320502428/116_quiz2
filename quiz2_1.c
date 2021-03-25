104438c2a6d395e2b1f8982b32dc7beab24fc028
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-25 17:10:46 +0700

    quiz2_1 up1

--- quiz2_1.c
+++ quiz2_1.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 int main ()
 {
+    int n,i,j,k=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        j=2;
+        while(j*j*j<i)
+        {
+            j++;
+            if(i%(j*j*j)!=0)
+            {
+                break;
+            }
+        }
+        if(i%(j*j*j)!=0)
+        {
+            k++;
+        }
+    }
     return 0;
 }
