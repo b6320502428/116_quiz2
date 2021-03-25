f7cb5b9ea34cc4bbce728a46a40ff4ce8f908228
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-25 18:20:38 +0700

    quiz2_2 up2

--- quiz2_2.c
+++ quiz2_2.c
@@ -14,6 +14,18 @@ int main ()
     for(i=0; i<n; i++)
     {
         scanf("%d %d %d",&p,&q,&r);
+        for(j=q-1;j<q+r-1;j++)
+        {
+            cloth[p-1][j]='x';
+        }
+    }
+    for(i=0; i<m; i++)
+    {
+        for(j=0; j<70; j++)
+        {
+            printf("%c",cloth[i][j]);
+        }
+        printf("\n");
     }
     return 0;
 }
